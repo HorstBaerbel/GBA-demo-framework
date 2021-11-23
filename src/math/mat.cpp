11f3264c6ee32032b14f08523555b196ee137f08
Bim Overbohm <bim.overbohm@googlemail.com>
2021-11-23 20:39:40 +0100

    Refactoring

--- src/math/mat.cpp
+++ src/math/mat.cpp
@@ -4,23 +4,23 @@ namespace Math
 {
 
         template <>
-        const mat2x2_t<Math::fp1616_t> mat2x2_t<Math::fp1616_t>::identity = {{1, 0}, {0, 1}};
+        const mat2x2_t<Math::fp1616_t> mat2x2_t<Math::fp1616_t>::identity EWRAM_DATA = {{1, 0}, {0, 1}};
         template <>
-        const mat2x2_t<Math::fp1616_t> mat2x2_t<Math::fp1616_t>::zero = {{0, 0}, {0, 0}};
+        const mat2x2_t<Math::fp1616_t> mat2x2_t<Math::fp1616_t>::zero EWRAM_DATA = {{0, 0}, {0, 0}};
 
         template <>
-        const mat3x3_t<Math::fp1616_t> mat3x3_t<Math::fp1616_t>::identity = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        const mat3x3_t<Math::fp1616_t> mat3x3_t<Math::fp1616_t>::identity EWRAM_DATA = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
         template <>
-        const mat3x3_t<Math::fp1616_t> mat3x3_t<Math::fp1616_t>::zero = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        const mat3x3_t<Math::fp1616_t> mat3x3_t<Math::fp1616_t>::zero EWRAM_DATA = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
         template <>
-        const mat3x4_t<Math::fp1616_t> mat3x4_t<Math::fp1616_t>::identity = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}};
+        const mat3x4_t<Math::fp1616_t> mat3x4_t<Math::fp1616_t>::identity EWRAM_DATA = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}};
         template <>
-        const mat3x4_t<Math::fp1616_t> mat3x4_t<Math::fp1616_t>::zero = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        const mat3x4_t<Math::fp1616_t> mat3x4_t<Math::fp1616_t>::zero EWRAM_DATA = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 
         template <>
-        const mat4x4_t<Math::fp1616_t> mat4x4_t<Math::fp1616_t>::identity = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+        const mat4x4_t<Math::fp1616_t> mat4x4_t<Math::fp1616_t>::identity EWRAM_DATA = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
         template <>
-        const mat4x4_t<Math::fp1616_t> mat4x4_t<Math::fp1616_t>::zero = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+        const mat4x4_t<Math::fp1616_t> mat4x4_t<Math::fp1616_t>::zero EWRAM_DATA = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 
 }
