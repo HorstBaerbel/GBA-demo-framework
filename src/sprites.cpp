b358544752d1a947322c7529b798e81edf1e3921
Bim Overbohm <bim.overbohm@googlemail.com>
2021-11-24 17:07:53 +0100

    Refactor sprites / tiles

--- src/sprites.cpp
+++ src/sprites.cpp
@@ -1,68 +1,66 @@
 #include "sprites.h"
-#include "memory/memory.h"
+
+#include "memory.h"
+
 #include <gba_affine.h>
 #include <gba_sprites.h>
 
 namespace Sprites
 {
 
-    const uint8_t TileCountForSizeCode[12] = {1, 4, 16, 64, 2, 4, 8, 32, 2, 4, 8, 32};
-    const uint8_t HorizontalTilesForSizeCode[12] = {1, 2, 4, 8, 2, 4, 4, 8, 1, 1, 2, 4};
-    const uint8_t VerticalTilesForSizeCode[12] = {1, 2, 4, 8, 1, 1, 2, 4, 2, 4, 4, 8};
-
     void clear()
     {
-        if (REG_DISPCNT & MODE_3 || REG_DISPCNT & MODE_4)
+        for (uint32_t i = 0; i < 1024; i++)
         {
-            Memory::memset32(&OAM[512], 0, sizeof(OBJATTR) * 512 / 4);
-        }
-        else
-        {
-            Memory::memset32(OAM, 0, sizeof(OBJATTR) * 1024 / 4);
+            OBJATTR *attribute = &OAM[i];
+            attribute->attr0 = OBJ_DISABLE;
+            attribute->attr1 = 0;
+            attribute->attr2 = 0;
+            attribute->dummy = 0;
         }
     }
 
-    uint16_t getSpriteScale(SizeCode size)
+    uint16_t getSpriteScale(Tiles::SizeCode size)
     {
         switch (size)
         {
-        case SizeCode::Size16x8:
-        case SizeCode::Size32x8:
-        case SizeCode::Size32x16:
-        case SizeCode::Size64x32:
+        case Tiles::SizeCode::Size16x8:
+        case Tiles::SizeCode::Size32x8:
+        case Tiles::SizeCode::Size32x16:
+        case Tiles::SizeCode::Size64x32:
             return OBJ_SIZE(0b01);
-        case SizeCode::Size8x16:
-        case SizeCode::Size8x32:
-        case SizeCode::Size16x32:
-        case SizeCode::Size32x64:
+        case Tiles::SizeCode::Size8x16:
+        case Tiles::SizeCode::Size8x32:
+        case Tiles::SizeCode::Size16x32:
+        case Tiles::SizeCode::Size32x64:
             return OBJ_SIZE(0b10);
         default:
             return 0;
         }
     }
 
-    uint16_t getSpriteSize(SizeCode size)
+    uint16_t getSpriteSize(Tiles::SizeCode size)
     {
         switch (size)
         {
-        case SizeCode::Size16x16:
-        case SizeCode::Size32x8:
-        case SizeCode::Size8x32:
+        case Tiles::SizeCode::Size16x16:
+        case Tiles::SizeCode::Size32x8:
+        case Tiles::SizeCode::Size8x32:
             return OBJ_SIZE(0b01);
-        case SizeCode::Size32x32:
-        case SizeCode::Size32x16:
-        case SizeCode::Size16x32:
+        case Tiles::SizeCode::Size32x32:
+        case Tiles::SizeCode::Size32x16:
+        case Tiles::SizeCode::Size16x32:
             return OBJ_SIZE(0b10);
-        case SizeCode::Size64x64:
-        case SizeCode::Size64x32:
-        case SizeCode::Size32x64:
+        case Tiles::SizeCode::Size64x64:
+        case Tiles::SizeCode::Size64x32:
+        case Tiles::SizeCode::Size32x64:
             return OBJ_SIZE(0b11);
         default:
             return 0;
         }
     }
 
-    void create(Sprite2D &sprite, uint16_t index, uint16_t x, uint16_t y, uint16_t tileIndex, SizeCode size, uint8_t paletteIndex, ColorDepth depth, Mode mode, bool mosaic, bool visible)
+    void create(Sprite2D &sprite, uint16_t index, int16_t x, int16_t y, uint16_t tileIndex, Tiles::SizeCode size, uint8_t paletteIndex, Tiles::ColorDepth depth, Mode mode, bool mosaic, bool visible)
     {
         sprite.index = index;
         sprite.x = x;
@@ -75,24 +73,34 @@ namespace Sprites
         sprite.visible = visible;
         sprite.type = Type::Regular;
         sprite.mode = mode;
+        sprite.mirrorH = false;
+        sprite.mirrorV = false;
+        sprite.priority = Tiles::Priority::PRIO_0;
+        sprite.doubleSize = false;
         sprite.matrixIndex = 0;
         sprite.matrix = Math::fp1616mat2x2_t::identity;
     }
 
     void setSprite(const Sprite2D &sprite)
     {
-        OBJATTR *attribute = &OAM[sprite.index];
+        auto attribute = &OAM[sprite.index];
         attribute->attr0 = OBJ_Y(sprite.y) | getSpriteScale(sprite.size);
-        attribute->attr0 |= (sprite.visible ? 0 : OBJ_DISABLE) | (sprite.mosaic ? OBJ_MOSAIC : 0) | (sprite.depth == ColorDepth::Depth256 ? ATTR0_COLOR_256 : ATTR0_COLOR_16);
+        attribute->attr0 |= (sprite.visible ? 0 : OBJ_DISABLE) | (sprite.mosaic ? OBJ_MOSAIC : 0) | (sprite.depth == Tiles::ColorDepth::Depth256 ? ATTR0_COLOR_256 : ATTR0_COLOR_16);
         attribute->attr0 |= (sprite.mode == Mode::Transparent ? OBJ_TRANSLUCENT : 0) | (sprite.mode == Mode::Window ? OBJ_OBJWINDOW : 0);
         attribute->attr1 = OBJ_X(sprite.x) | getSpriteSize(sprite.size);
         if (sprite.type == Type::Affine)
         {
-            attribute->attr0 |= (sprite.visible ? OBJ_ROT_SCALE_ON | OBJ_DOUBLE : 0);
-            attribute->attr1 |= ATTR1_ROTDATA(sprite.matrixIndex & 0x1F);
+            attribute->attr0 |= (sprite.visible ? OBJ_ROT_SCALE_ON : 0) | (sprite.doubleSize ? OBJ_DOUBLE : 0);
+            attribute->attr1 |= ATTR1_ROTDATA(static_cast<uint16_t>(sprite.matrixIndex) & 0x1F);
             setMatrix(sprite);
         }
-        attribute->attr2 = (sprite.tileIndex & 1023) | ATTR2_PALETTE(sprite.depth == ColorDepth::Depth256 ? 0 : (sprite.paletteIndex & 15));
+        else
+        {
+            attribute->attr1 |= (sprite.mirrorH ? OBJ_HFLIP : 0) | (sprite.mirrorV ? OBJ_VFLIP : 0);
+        }
+        attribute->attr2 = (sprite.tileIndex & 1023);
+        attribute->attr2 |= ATTR2_PALETTE(sprite.depth == Tiles::ColorDepth::Depth256 ? 0 : (sprite.paletteIndex & 15));
+        attribute->attr2 |= ATTR2_PRIORITY(static_cast<uint16_t>(sprite.priority));
     }
 
     void setSprites(const Sprite2D *sprites, uint32_t start, uint32_t count)
@@ -105,20 +113,27 @@ namespace Sprites
 
     void setVisible(const Sprite2D &sprite)
     {
-        OBJATTR *attribute = &OAM[sprite.index];
-        attribute->attr0 = (attribute->attr0 & 0b1111110011111111) | (sprite.visible ? 0 : OBJ_DISABLE) | ((sprite.type == Type::Affine && sprite.visible) ? OBJ_ROT_SCALE_ON | OBJ_DOUBLE : 0);
+        auto attribute = &OAM[sprite.index];
+        if (sprite.type == Type::Affine)
+        {
+            attribute->attr0 = (attribute->attr0 & 0b1111111011111111) | (sprite.visible ? OBJ_ROT_SCALE_ON : 0);
+        }
+        else
+        {
+            attribute->attr0 = (attribute->attr0 & 0b1111110111111111) | (sprite.visible ? 0 : OBJ_DISABLE);
+        }
     }
 
     void setPosition(const Sprite2D &sprite)
     {
-        OBJATTR *attribute = &OAM[sprite.index];
+        auto attribute = &OAM[sprite.index];
         attribute->attr0 = (attribute->attr0 & 0b1111111100000000) | OBJ_Y(sprite.y);
         attribute->attr1 = (attribute->attr1 & 0b1111111000000000) | OBJ_X(sprite.x);
     }
 
     void setMatrix(const Sprite2D &sprite)
     {
-        OBJAFFINE *objAffine = reinterpret_cast<OBJAFFINE *>(&OAM[sprite.matrixIndex & 0x1F]);
+        auto objAffine = &reinterpret_cast<OBJAFFINE *>(OAM)[sprite.matrixIndex & 0x1F];
         objAffine->pa = (int16_t)(sprite.matrix[0][0].raw() >> 8);
         objAffine->pb = (int16_t)(sprite.matrix[0][1].raw() >> 8);
         objAffine->pc = (int16_t)(sprite.matrix[1][0].raw() >> 8);
@@ -127,39 +142,39 @@ namespace Sprites
 
     void copyTileData(const Sprite2D &sprite, const Tiles::Tile16 *tileData)
     {
-        copyTileData(sprite, tileData);
+        copyTileData(&sprite, tileData, 1);
     }
 
     void copyTileData(const Sprite2D *sprites, const Tiles::Tile16 *tileData, uint32_t nrOfSprites)
     {
         uint32_t offsetInSrcData = 0;
-        auto srcData = reinterpret_cast<const uint8_t *>(tileData);
+        const uint8_t *srcData = reinterpret_cast<const uint8_t *>(tileData);
         for (uint32_t i = 0; i < nrOfSprites; ++i)
         {
             const auto &sprite = sprites[i];
-            const uint32_t bytesPerSprite = (uint32_t(TileCountForSizeCode[(uint32_t)sprite.size]) * 64) >> 1;
-            Memory::memcpy32(&SpriteMem16[sprite.tileIndex], srcData + offsetInSrcData, bytesPerSprite >> 2);
+            const uint32_t bytesPerSprite = (uint32_t(Tiles::TileCountForSizeCode[(uint32_t)sprite.size]) * 64) >> 1;
+            Memory::memcpy32(Sprites::TILE_INDEX_TO_MEM16(sprite.tileIndex), srcData + offsetInSrcData, bytesPerSprite >> 2);
             offsetInSrcData += bytesPerSprite;
         }
     }
 
     void copyTileData(const Sprite2D &sprite, const Tiles::Tile256 *tileData)
     {
-        uint32_t tileCount = TileCountForSizeCode[(uint32_t)sprite.size];
-        Memory::memcpy32(&SpriteMem256[sprite.tileIndex], tileData, sizeof(Tiles::Tile256) / 4 * tileCount);
+        uint32_t tileCount = Tiles::TileCountForSizeCode[(uint32_t)sprite.size];
+        Memory::memcpy32(Sprites::TILE_INDEX_TO_MEM256(sprite.tileIndex), tileData, sizeof(Tiles::Tile256) / 4 * tileCount);
     }
 
     void copyTileData(const Sprite2D *sprites, uint32_t nrOfSprites, const Tiles::Tile256 *bitmapData, uint32_t bitmapWidth, uint32_t xStep, uint32_t yStep)
     {
         // we copy 4 8bit pixels (1/2 tile line) at once
         const uint32_t tileDataStride = bitmapWidth >> 2;
-        auto tileData = reinterpret_cast<const uint32_t *>(bitmapData);
+        const uint32_t *tileData = reinterpret_cast<const uint32_t *>(bitmapData);
         for (uint32_t i = 0; i < nrOfSprites; ++i)
         {
             const auto &sprite = sprites[i];
-            const uint32_t copiesPerLine = HorizontalTilesForSizeCode[(uint32_t)sprite.size] << 1;
-            const uint32_t nrOfLines = VerticalTilesForSizeCode[(uint32_t)sprite.size] << 3;
-            auto spriteData = reinterpret_cast<uint32_t *>(&SpriteMem256[sprite.tileIndex]);
+            const uint32_t copiesPerLine = Tiles::HorizontalTilesForSizeCode[(uint32_t)sprite.size] << 1;
+            const uint32_t nrOfLines = Tiles::VerticalTilesForSizeCode[(uint32_t)sprite.size] << 3;
+            uint32_t *spriteData = reinterpret_cast<uint32_t *>(Sprites::TILE_INDEX_TO_MEM256(sprite.tileIndex));
             const uint32_t *srcData = tileData + (i * (xStep >> 2)) + (i * (yStep >> 2) * tileDataStride);
             for (uint32_t y = 0; y < nrOfLines; ++y)
             {
@@ -178,7 +193,7 @@ namespace Sprites
 
     void copyPalette(const Sprite2D &sprite, const Palette::Palette16 *paletteData, uint32_t nrOfPalettes, uint32_t nrOfEntries)
     {
-        Memory::memcpy16(Palette::SpritePaletteMem16[0][sprite.paletteIndex], paletteData, nrOfEntries * nrOfPalettes);
+        Memory::memcpy16(Palette::SpritePaletteMem16[sprite.paletteIndex], paletteData, nrOfEntries * nrOfPalettes);
     }
 
     void copyPalette(const Sprite2D &sprite, const Palette::Palette256 *paletteData, uint32_t nrOfEntries)
--- src/tiles.h
+++ src/tiles.h
@@ -2,23 +2,21 @@
 
 #include "palette.h"
 
-/// @brief Functions for tile-base backgrounds in modes 0/1/2
+// Functions for tile-base backgrounds in modes 0/1/2.
 namespace Tiles
 {
 
-    /// @brief s-tile 8x8@4bpp: 32 bytes, 8 words
     struct Tile16
     {
         uint32_t data[8];
-    };
+    }; //!<s-tile 8x8@4bpp: 32bytes; 8 uints
 
-    /// @brief d-tile: 8x8@8bpp: 64 bytes, 16 words
     struct Tile256
     {
         uint32_t data[16];
-    };
+    }; //!<d-tile: 8x8@8bpp: 64bytes; 16 uints
 
-    /// @brief Start of tile memory (== start VRAM)
+    /// @brief Start of tile memory (== start of VRAM)
     constexpr uint32_t TileMem = 0x06000000;
 
     /// @brief Tile memory interpreted as 16 color tiles
@@ -27,91 +25,128 @@ namespace Tiles
     /// @brief Tile memory interpreted as 256 color tiles
     auto const TileMem256{reinterpret_cast<Tile256 *>(TileMem)};
 
-    /// @brief This is where the tiles aka the bitmap / pixel data for the tiles starts
-    enum class TileBase : uint16_t
+    enum class SizeCode : uint8_t
     {
-        Base_0000 = (0 << 2),
-        Base_4000 = (1 << 2),
-        Base_8000 = (2 << 2),
-        Base_C000 = (3 << 2)
+        Size8x8 = 0,
+        Size16x16 = 1,
+        Size32x32 = 2,
+        Size64x64 = 3, //square, OBJ_SHAPE(0)
+        Size16x8 = 4,
+        Size32x8 = 5,
+        Size32x16 = 6,
+        Size64x32 = 7, //horizontal, OBJ_SHAPE(1)
+        Size8x16 = 8,
+        Size8x32 = 9,
+        Size16x32 = 10,
+        Size32x64 = 11 //vertical, OBJ_SHAPE(2)
     };
 
-    /// @brief Convert a tile base value to a VRAM address
+    enum class Background : uint8_t
+    {
+        BG0 = 0,
+        BG1 = 1,
+        BG2 = 2,
+        BG3 = 3
+    };
+
+    enum class Priority : uint8_t
+    {
+        PRIO_0 = 0,
+        PRIO_1 = 1,
+        PRIO_2 = 2,
+        PRIO_3 = 3
+    };
+
+    /// @brief This is where the tiles aka the bitmap data for the tiles starts.
+    enum class TileBase : uint16_t
+    {
+        BASE_0000 = (0 << 2),
+        BASE_4000 = (1 << 2),
+        BASE_8000 = (2 << 2),
+        BASE_C000 = (3 << 2)
+    };
+
     constexpr uint16_t *TILE_BASE_TO_MEM(TileBase b)
     {
         return reinterpret_cast<uint16_t *>(TileMem + ((uint32_t(b)) << 12));
     }
 
-    /// @brief This is where the screen / tile layout aka the map data for the tiles starts
+    /// @brief This is where the screen aka the map data for the tiles starts.
     enum class ScreenBase : uint16_t
     {
-        Base_0000 = (0 << 8),
-        Base_0800 = (1 << 8),
-        Base_1000 = (2 << 8),
-        Base_1800 = (3 << 8),
-        Base_2000 = (4 << 8),
-        Base_2800 = (5 << 8),
-        Base_3000 = (6 << 8),
-        Base_3800 = (7 << 8),
-        Base_4000 = (8 << 8),
-        Base_4800 = (9 << 8),
-        Base_5000 = (10 << 8),
-        Base_5800 = (11 << 8),
-        Base_6000 = (12 << 8),
-        Base_6800 = (13 << 8),
-        Base_7000 = (14 << 8),
-        Base_7800 = (15 << 8),
-        Base_8000 = (16 << 8),
-        Base_8800 = (17 << 8),
-        Base_9000 = (18 << 8),
-        Base_9800 = (19 << 8),
-        Base_A000 = (20 << 8),
-        Base_A800 = (21 << 8),
-        Base_B000 = (22 << 8),
-        Base_B800 = (23 << 8),
-        Base_C000 = (24 << 8),
-        Base_C800 = (25 << 8),
-        Base_D000 = (26 << 8),
-        Base_D800 = (27 << 8),
-        Base_E000 = (28 << 8),
-        Base_E800 = (29 << 8),
-        Base_F000 = (30 << 8),
-        Base_F800 = (31 << 8)
+        BASE_0000 = (0 << 8),
+        BASE_0800 = (1 << 8),
+        BASE_1000 = (2 << 8),
+        BASE_1800 = (3 << 8),
+        BASE_2000 = (4 << 8),
+        BASE_2800 = (5 << 8),
+        BASE_3000 = (6 << 8),
+        BASE_3800 = (7 << 8),
+        BASE_4000 = (8 << 8),
+        BASE_4800 = (9 << 8),
+        BASE_5000 = (10 << 8),
+        BASE_5800 = (11 << 8),
+        BASE_6000 = (12 << 8),
+        BASE_6800 = (13 << 8),
+        BASE_7000 = (14 << 8),
+        BASE_7800 = (15 << 8),
+        BASE_8000 = (16 << 8),
+        BASE_8800 = (17 << 8),
+        BASE_9000 = (18 << 8),
+        BASE_9800 = (19 << 8),
+        BASE_A000 = (20 << 8),
+        BASE_A800 = (21 << 8),
+        BASE_B000 = (22 << 8),
+        BASE_B800 = (23 << 8),
+        BASE_C000 = (24 << 8),
+        BASE_C800 = (25 << 8),
+        BASE_D000 = (26 << 8),
+        BASE_D800 = (27 << 8),
+        BASE_E000 = (28 << 8),
+        BASE_E800 = (29 << 8),
+        BASE_F000 = (30 << 8),
+        BASE_F800 = (31 << 8)
     };
 
-    /// @brief Convert a screen base value to a VRAM address
     constexpr uint16_t *SCREEN_BASE_TO_MEM(ScreenBase b)
     {
         return reinterpret_cast<uint16_t *>(TileMem + ((uint32_t(b)) << 3));
     }
 
-    /// @brief Tiled background screen size
     enum class ScreenSize : uint16_t
     {
-        Size_0 = (0 << 14), // Text mode: 256x256, Rotation/Scaling mode: 128x128
-        Size_1 = (1 << 14), // Text mode: 512x256, Rotation/Scaling mode: 256x256
-        Size_2 = (2 << 14), // Text mode: 256x512, Rotation/Scaling mode: 512x512
-        Size_3 = (3 << 14)  // Text mode: 512x512, Rotation/Scaling mode: 1024x1024
+        SIZE_0 = (0 << 14),
+        SIZE_1 = (1 << 14),
+        SIZE_2 = (2 << 14),
+        SIZE_3 = (3 << 14)
     };
 
-    /// @brief Build tile background config
-    constexpr uint16_t backgroundConfig(TileBase tileBase, ScreenBase screenBase, ScreenSize screenSize = ScreenSize::Size_0, uint16_t paletteColors = 16, uint16_t priority = 0, bool mosaic = false)
+    enum class ColorDepth : uint8_t
     {
-        return static_cast<uint16_t>(tileBase) | static_cast<uint16_t>(screenBase) | static_cast<uint16_t>(screenSize) | (paletteColors <= 16 ? 0 : (1 << 7)) | (priority & 3);
-    }
+        Depth16 = 0,
+        Depth256 = 1
+    };
 
-    /// @brief Copy tile data for multiple tiles from single bitmap to VRAM.
-    /// Use your if your sprites / tiles come from one bitmap.
-    //void copyTileData(Tile16 *tileMem, SizeCode blockSize, uint32_t nrOfBlocks, const Tile16 *bitmap, uint32_t bitmapWidth, uint32_t xStep = 0, uint32_t yStep = 0);
+    extern const uint8_t TileCountForSizeCode[12];
+    extern const uint8_t HorizontalTilesForSizeCode[12];
+    extern const uint8_t VerticalTilesForSizeCode[12];
+
+    /// @brief Set up background.
+    /// See: http://problemkaputt.de/gbatek.htm#lcdiobgcontrol
+    void setBackground(Background background, TileBase tileBase, ScreenBase screenBase, ScreenSize screenSize, ColorDepth depth, Priority priority = Priority::PRIO_0);
+
+    /// @brief Copy tile data for multiple tiles from bitmap to VRAM.
+    /// Use your if your tiles come from one bitmap.
+    void copyTileData(Tile16 *tileMem, SizeCode blockSize, uint32_t nrOfBlocks, const Tile16 *bitmap, uint32_t bitmapWidth, uint32_t xStep = 0, uint32_t yStep = 0);
 
     /// @brief Write random values to tile data.
     void randomTileData(Tile16 *tileMem, uint32_t nrOfTiles, bool noZeroColor = false);
 
     /// @brief Write random values to map data for text (NON rotate/scale backgrounds).
-    /// @param mapMem Map memory to fill.
-    /// @param nrOfTiles Number of map entries to fill. Must be divisible by 2!
+    /// @param mapMem Map meory to fill.
+    /// @param nrOfTiles Number of map entries to fill. Must be divisible by 2.
     /// @param paletteIndex Palette index for 16-color modes.
     /// @param pow2ModValue Power-of-2 modulu value for random values. Must be in [1,2,4,8,16,32,64,128,256,512,1024]!
     void randomMapData(uint16_t *mapMem, uint32_t nrOfTiles, uint8_t paletteIndex = 0, uint32_t pow2ModValue = 1024);
 
-} // namespace Tiles
+}
