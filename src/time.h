b295f2f013ba2f9a1c69ce53b169edb25f30c720
Bim Overbohm <bim.overbohm@googlemail.com>
2025-12-22 22:47:49 +0100

    Use modern declarations

--- src/time.h
+++ src/time.h
@@ -6,14 +6,14 @@ namespace Time
 {
 
 	/// @brief Enable timer. now() value increases every ~1ms now.
-	void start();
+	auto start() -> void;
 
 	/// @brief Stop timer. now() value will not increase anymore.
-	void stop();
+	auto stop() -> void;
 
 	/// @brief Return time since timer was started.
 	/// @return Returns the current time in seconds in 16:16 format.
 	/// @note This will wrap around ~4,5 hours after start() was called.
-	int32_t now();
+	auto now() -> int32_t;
 
 } // namespace Time
