30b9490a20a828590f2d0fc50441e1ad862b5899
Bim Overbohm <bim.overbohm@googlemail.com>
2026-01-03 17:16:48 +0100

    Rework input handling

--- src/sys/input.cpp
+++ src/sys/input.cpp
@@ -1,52 +1,84 @@
 #include "input.h"
 
-typedef struct
+#include "sys/halt.h"
+#include "sys/interrupts.h"
+
+namespace Input
 {
-    uint16_t Up;
-    uint16_t Down;
-    uint16_t Held;
-    uint16_t Last;
-    uint16_t DownRepeat;
-} __attribute__((packed)) KeyInput;
-
-static KeyInput Keys = {0, 0, 0, 0, 0};
-
-static constexpr uint8_t delay = 60;
-static constexpr uint8_t repeat = 30;
-static uint8_t count = 60;
-
-//---------------------------------------------------------------------------------
-void scanKeys(void)
-//---------------------------------------------------------------------------------
-{
-    Keys.Last = Keys.Held;
-    Keys.Held = (REG_KEYINPUT & 0x03ff) ^ 0x03ff; // upper 6 bits clear on hw not emulated
-    uint16_t pressed = Keys.Held & (Keys.Last ^ 0x03ff);
-    Keys.DownRepeat |= pressed;
-    Keys.Down |= pressed;
-    uint16_t released = ((Keys.Held ^ 0x03ff) & Keys.Last);
-    Keys.Up |= released;
-    Keys.Down &= ~released;
-    Keys.DownRepeat &= ~released;
-    Keys.Up &= ~pressed;
-    if (Keys.Last != Keys.Held)
+    struct KeyInput
     {
-        count = delay;
-    }
-    if (delay != 0)
+        uint16_t up;
+        uint16_t down;
+        uint16_t held;
+        uint16_t last;
+        uint16_t downRepeat;
+    } __attribute__((aligned(4), packed));
+
+    static KeyInput Keys = {0, 0, 0, 0, 0};
+    static constexpr uint8_t DelayCount = 60;
+    static constexpr uint8_t RepeatCount = 30;
+    static uint8_t heldCount = 60;
+    static constexpr uint16_t KeyBitsMask = 0x03FF; // Mask lower 10 bits of key input
+
+    auto scanKeys() -> void
     {
-        count--;
-        if (count == 0)
+        Keys.last = Keys.held;
+        Keys.held = (RegKeyInput & KeyBitsMask) ^ KeyBitsMask;
+        const uint16_t pressed = Keys.held & (Keys.last ^ KeyBitsMask);
+        Keys.downRepeat |= pressed;
+        Keys.down |= pressed;
+        const uint16_t released = ((Keys.held ^ KeyBitsMask) & Keys.last);
+        Keys.up |= released;
+        Keys.down &= ~released;
+        Keys.downRepeat &= ~released;
+        Keys.up &= ~pressed;
+        if (Keys.last != Keys.held)
         {
-            count = repeat;
-            Keys.DownRepeat |= Keys.Held;
+            heldCount = DelayCount;
+        }
+        if (DelayCount != 0)
+        {
+            heldCount--;
+            if (heldCount == 0)
+            {
+                heldCount = RepeatCount;
+                Keys.downRepeat |= Keys.held;
+            }
         }
     }
-}
 
-uint16_t keysDown(void)
-{
-    auto tmp = Keys.Down;
-    Keys.Down = 0;
-    return tmp;
+    auto keysDown() -> KeyBits
+    {
+        auto tmp = Keys.down;
+        Keys.down = 0;
+        return static_cast<KeyBits>(tmp);
+    }
+
+    auto waitForKeysDown(KeyBits keys, bool halt) -> void
+    {
+        do
+        {
+            scanKeys();
+            if (keysDown() & keys)
+            {
+                break;
+            }
+            if (halt)
+            {
+                // check if VBlank IRQ was set up and store old handler
+                const auto wasEnabled = (Irq::getMask() & Irq::Mask::VBlank) != 0;
+                const auto oldHandler = Irq::getHandler(Irq::Mask::VBlank);
+                // set dummy handler and enable VBlank IRQ
+                Irq::setHandler(Irq::Mask::VBlank, Irq::DummyHandler);
+                Irq::enable(Irq::Mask::VBlank);
+                Halt::Halt();
+                // restore old VBlank handler and enable flag
+                Irq::setHandler(Irq::Mask::VBlank, oldHandler);
+                if (!wasEnabled)
+                {
+                    Irq::disable(Irq::Mask::VBlank);
+                }
+            }
+        } while (true);
+    }
 }
--- src/sys/input.h
+++ src/sys/input.h
@@ -4,35 +4,45 @@
 
 #include "base.h"
 
-/// @brief Key Input register
-#define REG_KEYINPUT *(volatile uint16_t *)(REG_BASE + 0x130)
-
-/// @brief Keypad interrupt control register
-#define REG_KEYCNT *(volatile uint16_t *)(REG_BASE + 0x132)
-
-/// @brief Bit values for keypad buttons
-typedef enum KEYPAD_BITS
+namespace Input
 {
-    KEY_A = (1 << 0),                                 // keypad A button
-    KEY_B = (1 << 1),                                 // keypad B button
-    KEY_SELECT = (1 << 2),                            // keypad SELECT button
-    KEY_START = (1 << 3),                             // keypad START button
-    KEY_RIGHT = (1 << 4),                             // dpad RIGHT
-    KEY_LEFT = (1 << 5),                              // dpad LEFT
-    KEY_UP = (1 << 6),                                // dpad UP
-    KEY_DOWN = (1 << 7),                              // dpad DOWN
-    KEY_R = (1 << 8),                                 // Right shoulder button
-    KEY_L = (1 << 9),                                 // Left shoulder button
-    KEYIRQ_ENABLE = (1 << 14),                        // Enable keypad interrupt
-    KEYIRQ_OR = (0 << 15),                            // interrupt logical OR mode
-    KEYIRQ_AND = (1 << 15),                           // interrupt logical AND mode
-    DPAD = (KEY_UP | KEY_DOWN | KEY_LEFT | KEY_RIGHT) // mask all dpad buttons
-} KEYPAD_BITS;
+    /// @brief Register for key input
+    inline auto &RegKeyInput{*reinterpret_cast<volatile uint16_t *>(REG_BASE + 0x130)};
 
-/// @brief Call this function once per main loop in order to use the keypad functions
-void scanKeys(void);
+    /// @brief Keypad interrupt control register
+    inline auto &RegKeyCnt{*reinterpret_cast<volatile uint16_t *>(REG_BASE + 0x132)};
 
-/// @brief Obtain the current keypad pressed state.
-/// Returns the keys which have been pressed since the last call to KeysDown(), the keys are reset on this call.
-/// Keys which are pressed will not be reported again until they are released.
-uint16_t keysDown();
+    /// @brief Bit values for keypad buttons
+    enum KeyBits : uint16_t
+    {
+        KeyA = (1 << 0),        // Keypad A button
+        KeyB = (1 << 1),        // Keypad B button
+        KeySelect = (1 << 2),   // Keypad select button
+        KeyStart = (1 << 3),    // Keypad start button
+        KeyRight = (1 << 4),    // Dpad right
+        KeyLeft = (1 << 5),     // Dpad left
+        KeyUp = (1 << 6),       // Dpad up
+        KeyDown = (1 << 7),     // Dpad down
+        KeyR = (1 << 8),        // Right shoulder button
+        KeyL = (1 << 9),        // Left shoulder button
+        IrqEnable = (1 << 14),  // Enable keypad interrupt (only use with RegKeyCnt)
+        IrqCondOr = (0 << 15),  // Interrupt when one of the selected buttons is pressed (only use with RegKeyCnt)
+        IrqCondAnd = (1 << 15), // Interrupt when all of the selected buttons are pressed (only use with RegKeyCnt)
+    };
+
+    /// @brief Any Dpad button
+    constexpr KeyBits KeysDpad = static_cast<KeyBits>(KeyBits::KeyUp | KeyBits::KeyDown | KeyBits::KeyLeft | KeyBits::KeyRight);
+
+    /// @brief Call this function once per main loop in order to use the keypad functions
+    auto scanKeys() -> void;
+
+    /// @brief Obtain the current keypad pressed state
+    /// @return Returns the keys which have been pressed since the last call to keysDown(), the keys are reset on this call.
+    /// Keys which are pressed will not be reported again until they are released.
+    auto keysDown() -> KeyBits;
+
+    /// @brief Wait for specific keys to be pressed
+    /// @param keys Keys to be pressed
+    /// @param halt If true, halt CPU to save power. If false, busy-wait.
+    auto waitForKeysDown(KeyBits keys, bool halt = false) -> void;
+}
